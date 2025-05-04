0e7755ff7eabd7ae2993970cdaa11e8292225542
Hsouna-M <Hsouna.mohamedaziz@gmail.com>
2025-05-04 10:17:44 +0100

    created and tested the connextion class

--- src/database/connexion.php
+++ src/database/connexion.php
@@ -1,16 +1,46 @@
 <?php
-require 'config/dbconfig.php' ; 
-// charset is important it caused an error without it . "mysql:host=localhost;dbname=dbname;chrset=utf8mb4"
 
-$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
-try{
-    $pdo = new PDO($dsn, $user , $password) ;
-    //set the pdo error mode to exception so i can catch below 
+class Connexion{
+   private $dbname="gestion_stage";    
+   private $user="admin_gestion_stage";
+   private $password="password";
+   private $host="localhost";
+   private $charset='utf8mb4' ; 
 
-}catch(PDOException $e){
-    // accessing a method in the $e object
-    echo 'Error: '.$e->getMessage().'<br>' ;
+
+   public function connexion(){
+       
+        try{
+            $dsn = 'mysql:host'. $this->host . ';dbname=' . $this->dbname;
+
+            $pdo_conn = new PDO($dsn, $this->user , $this->password) ;
+
+            //set the pdo error mode to exception so i can catch below 
+            $pdo_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+          
+            // set teh fetch mode to fetch_assoc to get a table as a result when using the $pdo_conn->fetchall()
+            $pdo_conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
+           
+            return $pdo_conn ;
+        
+        }catch(PDOException $e){
+         
+            // accessing a method in the $e object
+            echo 'Error Connection to the DB: '.$e->getMessage().'<br>' ;
+
+        }
+    }
 }
 
+// testcondition 
+$connexion = new Connexion();
+
+$pdo_connexion = $connexion->connexion();
+
+if ($pdo_connexion) {
+
+    echo "connexion success" ; 
+
+}else{echo "problem";}
 
 ?>
--- src/database/connexion_classique.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../config/dbconfig.php' ; 
-
-$conn = mysqli_connect($host , $user , $password , $dbname) ; 
-echo 'connexion succeded <br>';
-if (! $conn) {
-    die('connexion failed'.mysqli_connect_error()) ; 
-}
-?>
